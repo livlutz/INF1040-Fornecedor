a8fc2e321965b1c267faf666478d25c7a68ff390
Livia <livialutz385@gmail.com>
2023-06-18 23:42:39 -0300

    Testes mudados e novos arquivos
    O arquivo testes.JSON foi alterado e todas as instruções se encontram nos comentários do código

--- testesJSON.py
+++ testesJSON.py
@@ -1,85 +1,104 @@
-#importando dados do arquivo JSON
-
+#Importando dados do arquivo JSON
 import json
+import os.path
 from jogos import *
 
+#precoPadrao é o preço que sera acrescentado ao jogo que tiver preferencia pela locadora e que nao estiver presente no estoque
+precoPadrao = 29.99
+
+#Usaremos 3 Arquivos: de pedido/requerimento com o nome do jogo 
+# de resposta com o stauts se o jogo pedido existe ou não no estoque da fornecedora
+# e de preferência com o nome do jogo que terá a preferência pela locadora
+
+#OBS: Os nomes dos jogos usados tanto pela fornecedora quanto pela locadora estão em capslock e sem acentos
+
+#Função que le o arquivo JSON de pedido com o nome do jogo que sera buscado
+
+def receberPedido():
+    #Verificando se o arquivo existe
+    file_exists = os.path.isfile('requerimentos.json')
+    
+    if(file_exists == False):
+        print("Arquivo de requerimentos inexixtente\n")
+    
+    else:
+        #Lendo dados do arquivo JSON de pedidos/ requerimentos com o nome do jogo 
+        with open('requerimentos.json','r') as requerimentos:
+            info = json.load(requerimentos)
+            
+            #Fechando o arquivo 
+            requerimentos.close()
+            
+            #info tem o nome do jogo
+            return info
+
+#Função que lê o arquivo JSON de preferência com o nome do jogo que terá uma preferência pela locadora
+
+def receberPreferencia():
+    #Verificando se o arquivo existe
+    file_exists = os.path.isfile('preferencia.json')
+    
+    if(file_exists == False):
+        print("Arquivo de preferencias inexistente\n")
+    
+    #Lendo dados do arquivo JSON de preferências com o nome do jogo
+    else:
+        with open('preferencia.json','r') as preferencia:
+            info = json.load(preferencia)
+            
+            #fechando o arquivo
+            preferencia.close()
+            
+            #info tem o nome do jogo
+            return info
+
+#Abrir arquivo json de estoque
+
 with open('derulo.json','r') as derulo:
+    #data['estoque'] é o dicionario que contem os jogos
     data = json.load(derulo)
+    
+    #Fechando o arquivo
+    derulo.close()
 
-#data['estoque'] é o dicionario que contem os jogos
+#Lendo o arquivo JSON de pedido
+info = receberPedido()
 
-#criando um menu para o usuario
+#info tem o nome do jogo a ser buscado
+info = info['nome']
 
-print("Bem vindo ao menu de jogos\n")
-print("Digite 1 para exibir o estoque\n")
-print("Digite 2 para buscar um jogo\n")
-print("Digite 3 para incluir um jogo no estoque\n")
-print("Digite 4 para excluir um jogo do estoque\n")
-print("Digite 5 para alterar dados de um jogo\n")
-print("Digite 6 para solicitar a compra de um jogo\n")
-print("Digite 7 para sair\n")
+#criando um arquivo JSON de resposta
+novo = open('resposta.json','w')
 
-opcao = int(input("Digite a opção desejada: "))
+#buscando o jogo no estoque
+jogo = buscaJogo(info,data['estoque'])   
 
-#Enquanto o usuario nao digitar 7, o menu continuará aparecendo
+#se o jogo for encontrado
+if(jogo != -1):
+    #escrevendo a mensagem de sucesso no arquivo JSON de resposta
+    novo.write("Jogo encontrado\n")
+    
+#se o jogo não for encontrado 
+else:
+    #escrevendo a mensagem de o jogo nao existe no estoque no arquivo JSON de resposta
+    novo.write("Jogo nao foi encontrado no estoque\n")
 
-while(opcao != 7):
-    #se o usuario digitar 1, o estoque será exibido
-    if(opcao == 1):
-        exibeEstoque(data['estoque'])
-        
-    #se o usuario digitar 2, o jogo será buscado
-    
-    elif(opcao == 2):
-        nome = input("Digite o nome do jogo: ")
-        buscaJogo(nome,data['estoque'])
-        
-    #se o usuario digitar 3, o jogo será incluido no estoque
-    
-    elif(opcao == 3):
-        print("Digite os dados do jogo a ser incluido\n")
-        nome = input("Digite o nome do jogo: ")
-        preco = float(input("Digite o preço do jogo: "))
-        quantidade = int(input("Digite a quantidade do jogo: "))
-        incluiJogo(data['estoque'],nome,preco,quantidade)
-    
-    #se o usuario digitar 4, o jogo será excluido do estoque
-    
-    elif(opcao == 4):
-        print("Digite os dados do jogo a ser excluido\n")
-        nome = input("Digite o nome do jogo: ")
-        excluiJogo(nome,data['estoque'])
-    
-    #se o usuario digitar 5, o jogo será alterado
-    
-    elif(opcao == 5):
-        print("Digite os dados do jogo a ser alterado\n")
-        nome = input("Digite o nome do jogo: ")
-        nomeNovo = input("Digite o novo nome do jogo: ")
-        preco = float(input("Digite o preço do jogo: "))
-        quantidade = int(input("Digite a quantidade do jogo: "))
-        alteraJogo(nome,nomeNovo,preco,quantidade,data['estoque'])
-    
-    #se o usuario digitar 6, o jogo será comprado
-    
-    elif(opcao == 6):
-        print("Digite os dados do jogo a ser comprado\n")
-        nome = input("Digite o nome do jogo: ")
-        valorPagar = float(input("Digite o valor a ser pago: "))
-        qtdCompra = int(input("Digite a quantidade a ser comprada: "))
-        solicitaCompra(nome,data['estoque'],valorPagar,qtdCompra)
-        
-        if(solicitaCompra(nome,data['estoque'],valorPagar,qtdCompra) == 1):
-           vendeJogo(nome,data['estoque'],valorPagar,qtdCompra)
-        
-    #se o usuario digitar 7, o programa será encerrado
-    opcao = int(input("Digite a opção desejada: "))
-    
-#salvando dados no arquivo JSON
+#Fecha o arquivo de resposta
+novo.close()
 
+#Lendo o arquivo JSON de preferência
+pref = receberPreferencia()
+
+#pref tem o nome do jogo a ter preferência pela locadora
+pref=pref['nome']
+
+#Acrescentando a preferência de um jogo no estoque
+preferenciaJogo(pref,precoPadrao,data['estoque'])
+
+#Atualizando o arquivo JSON de estoque
 with open('derulo.json','w') as derulo:
     json.dump(data,derulo,indent=4)
 
-#fechando o arquivo JSON
+#Fechando o arquivo JSON
 derulo.close()
 
